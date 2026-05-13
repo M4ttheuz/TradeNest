8d714514f624e17f2b0e61d2e452cec376fbd412
SzymonBogunia <s61273@student.ubb.edu.pl>
2026-05-14 01:05:23 +0200

    User Rating page frontend

---
+++ Controllers/UserController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TradeNest.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult UserRatings()
+        {
+            ViewData["Title"] = "Oceny użytkownika";
+            return View();
+        }
+    }
+}

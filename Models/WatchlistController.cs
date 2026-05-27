e48fa374739a262814180561612627e61b23de05
KosmaObuchowski <kosma3103@gmail.com>
2026-05-27 20:08:57 +0200

    watchlist placeholder

---
+++ Models/WatchlistController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TradeNest.Controllers
+{
+    public class WatchlistController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
